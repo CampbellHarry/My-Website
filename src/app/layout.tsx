89e94180385f8de84dc4e49324815c4b3eca15c0
CampbellHarry <hthebeast7@gmail.com>
2024-08-05 01:12:10 +0100

    keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Harry Campbell",
   description: "Harry Campbell is a software developer based in the UK. He has 3 years of experience working in web development and security engineering.",
+  keywords: ["Harry Campbell", "Harry", "Campbell", "livepool dev", "coder", "liverpool", "website developer", "best developer st helens", "website engineer", "st helens dev", "manchester developer", "web dev manchester", "full stack", "Hdev Group", "Hdev", "Group", "Software Developer", "Web Developer", "Security Engineer", "Full Stack Developer", "Frontend Developer", "Backend Developer", "Database Developer", "UK", "England", "United Kingdom", "Europe", "Europe", "Web Development", "Security Engineering"],
+
 };
 
 export default function RootLayout({
